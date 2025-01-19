a71e7deecc9f2e5c5e53b6a06460586824148fb4
Karaage <karaage01@outlook.jp>
2025-01-19 16:39:17 +0900

    apply format

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,81 +23,82 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  {
-    'sudormrfbin/cheatsheet.nvim',
-  },
-  {
-    'neoclide/coc.nvim', branch = 'release'
-  },
-  {
-    "github/copilot.vim", lazy = false,
-  }
-  ,
-  {
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
+    {
+        'sudormrfbin/cheatsheet.nvim',
     },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    {
+        'neoclide/coc.nvim', branch = 'release'
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    {
+        "github/copilot.vim", lazy = false,
     }
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("lualine").setup()
-    end,
-  },
-  {
-    "svrana/neosolarized.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("neosolarized").setup({
-        comment_italics = true,
-        background_set = false,
-      })
-      vim.cmd.colorscheme("neosolarized")
-    end,
-    dependencies = {
-      "tjdevries/colorbuddy.nvim",
-    },
-  },
-  {
-    'nvim-tree/nvim-tree.lua'
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    config = function()
-      require('telescope').setup({
-        defaults = {
-          file_ignore_patterns = {
-            "node_modules",
-          },
+    ,
+    {
+        'kdheepak/lazygit.nvim',
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
         }
-      })
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end,
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require("lualine").setup()
+        end,
+    },
+    {
+        "svrana/neosolarized.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("neosolarized").setup({
+                comment_italics = true,
+                background_set = false,
+            })
+            vim.cmd.colorscheme("neosolarized")
+        end,
+        dependencies = {
+            "tjdevries/colorbuddy.nvim",
+        },
+    },
+    {
+        'nvim-tree/nvim-tree.lua'
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        config = function()
+            require('telescope').setup({
+                defaults = {
+                    file_ignore_patterns = {
+                        "node_modules",
+                        "out",
+                    },
+                }
+            })
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        end,
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
