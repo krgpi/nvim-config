cdf4b69a2763024c88b2a10cc1797e4683402b25
Karaage <karaage01@outlook.jp>
2025-01-19 13:59:33 +0900

    add nvim-tree

--- init.lua
+++ init.lua
@@ -2,3 +2,13 @@ require("config.lazy")
 require("lualine").setup()
 
 vim.cmd.colorscheme('neosolarized')
+-- nvim-tree
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+})
+
