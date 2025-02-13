80cc9719d384e26638677e2092a7778de5af052c
Karaage <karaage01@outlook.jp>
2025-02-14 08:59:56 +0900

    move vim keymap config

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,3 +1,8 @@
+vim.keymap.set("n", "<M-j>", "<Cmd>move .+1<CR>==")
+vim.keymap.set("x", "<M-j>", ":move '>+1<CR>gv=gv")
+vim.keymap.set("n", "<M-k>", "<Cmd>move .-2<CR>==")
+vim.keymap.set("x", "<M-k>", ":move '<-2<CR>gv=gv")
+
 -- editor options
 vim.opt.list = true
 vim.opt.listchars = {
