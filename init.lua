21d9457f87d2748f66f8cd16fc522a7ffd99b487
Karaage <karaage01@outlook.jp>
2025-02-17 17:01:38 +0900

    extract file tree to the file I opened in the new buffer

--- init.lua
+++ init.lua
@@ -37,3 +37,14 @@ require("cheatsheet").setup({
         disabled = { 'nerd-fonts' },
     },
 })
+
+local api = require("nvim-tree.api")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if (vim.fn.bufname() == "NvimTree_1") then return end
+
+        api.tree.find_file({ buf = vim.fn.bufnr() })
+    end,
+})
